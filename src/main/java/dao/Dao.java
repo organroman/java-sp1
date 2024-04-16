124cc2b93541b10284c5c961c627d2055d5c066a
AlexDDD <dolzhikov.alexander1@gmail.com>
2024-04-16 13:05:51 +0300

    Update DAO body.

--- src/main/java/dao/Dao.java
+++ src/main/java/dao/Dao.java
@@ -1,4 +1,19 @@
 package dao;
 
-public interface Dao {
+import java.io.IOException;
+import java.util.List;
+
+public interface Dao<T> {
+    List<T> getAllFamilies();
+
+    T getById(int id);
+
+    boolean deleteEntity(int index);
+
+    boolean updateEntity(T y);
+
+
+    List<T> loadDataBase();
+
+    void saveDataBase(List<T> object) throws IOException;
 }

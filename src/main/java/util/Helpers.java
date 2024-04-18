49d35360733c332bf64c110d0d961c02eb19d21d
Roman Organ <r.organ@dan-it.com.ua>
2024-04-18 14:16:29 +0300

    returned deleted pom.xml, added methods: find flights and increase available seats

--- src/main/java/util/Helpers.java
+++ src/main/java/util/Helpers.java
@@ -3,10 +3,14 @@ package util;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 
 public class Helpers {
+
     public static boolean isSameDay(long dateTime, LocalDate dateToCompare) {
         LocalDate flightDate = Instant.ofEpochMilli(dateTime).atZone(ZoneId.systemDefault()).toLocalDate();
         return flightDate.equals(dateToCompare);
     }
+
+
 }

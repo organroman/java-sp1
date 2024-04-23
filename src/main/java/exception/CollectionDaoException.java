600d7e894edfee3738f8a2b410cf8c19ebfe217d
Roman Organ <r.organ@dan-it.com.ua>
2024-04-23 16:38:18 +0300

    one common DaoException, test for flights dao and controller

--- src/main/java/exception/CollectionDaoException.java
+++ src/main/java/exception/CollectionDaoException.java
@@ -9,6 +9,14 @@ public class CollectionDaoException extends RuntimeException {
     public static final String ERROR_LOADING_DATA = "Error in loading data";
     public static final String ERROR_SAVING_DATA = "Error in saving data";
 
+    public static final String ERROR_GET_ALL_ORDERS = "Error retrieving all orders";
+    public static final String ERROR_CREATING_NEW_ORDER = "Error creating new order";
+    public static final String ERROR_GETTING_ORDER = "Error getting the order";
+    public static final String ERROR_UPDATING_ORDER = "Error updating order";
+    public static final String ERROR_DELETE_ORDER = "Error delete order";
+    public static final String ERROR_LOADING_DATA_ORDERS = "Error in loading orders data";
+    public static final String ERROR_SAVING_DATA_ORDERS = "Error in saving orders data";
+
     public CollectionDaoException(String message) {
         super(message);
     }
